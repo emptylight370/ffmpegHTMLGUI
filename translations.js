2391a3cc04324172bff49033507c698489684f1a
Lingfengyu <1378990254@qq.com>
2023-08-21 21:18:26 +0800

    more translations

--- translations.js
+++ translations.js
@@ -98,35 +98,123 @@ var translations = {
         "warning001": {
             "en": "This item is required",
             "zh-CN": "此项必填",
+            "zh-TW": "此項必填",
+            "ja": "この項目は必須です",
         },
         "ph001": {
             "en": "Paste the file path here",
             "zh-CN": "这里粘贴文件的路径",
+            "zh-TW": "這裏粘貼文件的路徑",
+            "ja": "ここにファイルのパスを貼り付けます",
         },
         "ph002": {
             "en": "After generating the command, please copy it to the command line and run it, make sure that you have configured the ffmpeg environment and added it to the environment variable",
             "zh-CN": "在生成指令之后请复制到命令行中执行，请确保自己配置好了ffmpeg环境，并且已经添加到了环境变量中",
+            "zh-TW": "在生成指令之後請複製到命令行中執行，請確保自己配置好了ffmpeg環境，並且已經添加到了環境變量中",
+            "ja": "コマンドを生成した後、コマンドラインにコピーして実行してください。ffmpeg環境を設定し、環境変数に追加していることを確認してください",
         },
         "ph003": {
             "en": "Leave blank the default output in the input video folder",
             "zh-CN": "留空默认在输入视频文件夹输出",
+            "zh-TW": "留空默認在輸入視頻文件夾輸出",
+            "ja": "空白の場合、入力ビデオフォルダに出力されます",
+        },
+        "ph004": {
+            "en": "Leave blank to add ' _ffmpeg' after the file name",
+            "zh-CN": "留空默认在文件名后方加上' _ffmpeg'",
+            "zh-TW": "留空默認在文件名後方加上' _ffmpeg'",
+            "ja": "空白の場合、ファイル名の後に「_ffmpeg」が追加されます",
+        },
+        "ph005": {
+            "en": "Unit is kbit/s",
+            "zh-CN": "单位为kbit/s",
+            "zh-TW": "單位為kbit/s",
+            "ja": "単位はkbit/sです",
+        },
+        "ph006": {
+            "en": "Unit is fps",
+            "zh-CN": "单位为fps",
+            "zh-TW": "單位為fps",
+            "ja": "単位はfpsです",
         },
         "tip001": {
             "en": "You need to fill in the available folder address, please end with '\\'",
             "zh-CN": "需要填写可用的文件夹地址，请以'\\'结尾",
+            "zh-TW": "需要填寫可用的文件夾地址，請以'\\'結尾",
+            "ja": "使用可能なフォルダアドレスを入力する必要があります。 '\\'で終了してください",
         },
         "tip002": {
             "en": "Even if you only modify the suffix, you need to fill in the complete file name, including the suffix here",
             "zh-CN": "只修改后缀名也要填写完整文件名，这里的文件名包括后缀名",
+            "zh-TW": "只修改後綴名也要填寫完整文件名，這裏的文件名包括後綴名",
+            "ja": "拡張子のみを変更しても、ここで拡張子を含む完全なファイル名を入力する必要があります",
         },
         "tip003": {
             "en": "Video resolution, in pixels, format example 1920x1080, can be left blank, leaving blank defaults to the original resolution",
             "zh-CN": "视频分辨率，单位为像素，格式示例1920x1080，可以留空，留空默认原分辨率",
+            "zh-TW": "視頻分辨率，單位為像素，格式示例1920x1080，可以留空，留空默認原分辨率",
+            "ja": "ビデオ解像度、ピクセル単位、フォーマット例1920x1080、空白の場合はデフォルトで元の解像度になります",
         },
         "tip004": {
             "en": "Video bitrate, in kbit/s, leave blank to let ffmpeg decide",
             "zh-CN": "视频比特率，单位为kbit/s，留空默认由ffmpeg决定",
+            "zh-TW": "視頻比特率，單位為kbit/s，留空默認由ffmpeg決定",
+            "ja": "ビデオビットレート、kbit/s単位、空白の場合はffmpegが決定します",
         },
+        "tip005": {
+            "en": "Set video fps, leave blank to default to the same as the original video, unit is fps",
+            "zh-CN": "设置视频帧率，不填默认和原视频一样，单位为fps",
+            "zh-TW": "設置視頻幀率，不填默認和原視頻一樣，單位為fps",
+            "ja": "ビデオフレームレートを設定します。空白の場合は、元のビデオと同じになります。単位はfpsです",
+        },
+        "tip006": {
+            "en": "Select the output video quality, which will affect the video output speed. When it is not selected, no parameters will be added",
+            "zh-CN": "指定输出的视频质量，会影响视频输出速度，显示未选择时不会添加参数",
+            "zh-TW": "指定輸出的視頻質量，會影響視頻輸出速度，顯示未選擇時不會添加參數",
+            "ja": "出力ビデオ品質を選択します。これにより、ビデオの出力速度が変化します。選択されていない場合、パラメータは追加されません",
+        },
+        "tip007": {
+            "en": "This is the video decoder of the input video",
+            "zh-CN": "这里的视频解码器是输入的视频解码",
+            "zh-TW": "這裏的視頻解碼器是輸入的視頻解碼",
+            "ja": "ここでのビデオデコーダは入力ビデオのデコードです",
+        },
+        "tip008": {
+            "en": "Select video encoder, pay attention to whether the video encoder format here is suitable for FFmpeg in your current environment, mouse over the option to view the parameters used",
+            "zh-CN": "选择视频编码器，注意了解这里的视频编码器格式是否适用于你当前环境中的FFmpeg，鼠标悬停在选项上可查看使用的参数",
+            "zh-TW": "選擇視頻編碼器，注意了解這裏的視頻編碼器格式是否適用於你當前環境中的FFmpeg，鼠標懸停在選項上可查看使用的參數",
+            "ja": "ビデオエンコーダを選択します。ここでのビデオエンコーダフォーマットが現在の環境のFFmpegに適しているかどうかに注意してください。オプションにマウスを合わせて使用されるパラメータを表示します",
+        },
+        "tip009": {
+            "en": "Select audio encoder, pay attention to whether the audio encoder format here is suitable for FFmpeg in your current environment, mouse over the option to view the parameters used",
+            "zh-CN": "选择音频编码器，注意了解这里的音频编码器格式是否适用于你当前环境中的FFmpeg，鼠标悬停在选项上可查看使用的参数",
+            "zh-TW": "選擇音頻編碼器，注意了解這裏的音頻編碼器格式是否適用於你當前環境中的FFmpeg，鼠標懸停在選項上可查看使用的參數",
+            "ja": "オーディオエンコーダを選択します。ここでのオーディオエンコーダフォーマットが現在の環境のFFmpegに適しているかどうかに注意してください。オプションにマウスを合わせて使用されるパラメータを表示します",
+        },
+        "tip010": {
+            "en": "When this option is enabled, only the metadata of the video will be displayed, and the encoding progress will not be displayed",
+            "zh-CN": "当输出目录存在有与输出的文件同名的文件时询问是否覆盖，启用后不询问自动覆盖",
+            "zh-TW": "當輸出目錄存在有與輸出的文件同名的文件時詢問是否覆蓋，啓用後不詢問自動覆蓋",
+            "ja": "出力ディレクトリに、出力ファイルと同じ名前のファイルが存在する場合、上書きするかどうかを尋ねます。有効にすると、自動的に上書きされます",
+        },
+        "tip011": {
+            "en": "When this option is enabled, the encoding progress will be displayed during the encoding process",
+            "zh-CN": "开启后在编码过程中会显示编码进度",
+            "zh-TW": "開啓後在編碼過程中會顯示編碼進度",
+            "ja": "有効にすると、エンコード中にエンコード状態が表示されます",
+        },
+        "tip012": {
+            "en": "When this option is enabled, the output video will not be output, only the audio will be output",
+            "zh-CN": "启用后不输出视频，只输出音频，建议输出文件为音频格式",
+            "zh-TW": "啓用後不輸出視頻，只輸出音頻，建議輸出文件為音頻格式",
+            "ja": "有効にすると、ビデオを出力せず、オーディオのみを出力します。出力ファイルはオーディオ形式であることをお勧めします",
+        },
+        "tip013": {
+            "en": "When this option is enabled, the output audio will not be output, only the video will be output, advise the output file to be a video format",
+            "zh-CN": "启用后不输出音频，只输出视频，建议输出文件为视频格式",
+            "zh-TW": "啓用後不輸出音頻，只輸出視頻，建議輸出文件為視頻格式",
+            "ja": "有効にすると、オーディオを出力せず、ビデオのみを出力します。出力ファイルはビデオ形式であることをお勧めします",
+        }
     },
     "video": {
         "video_resolution_label": {
@@ -156,6 +244,8 @@ var translations = {
         "video_decoder_label": {
             "en": "Select video decoder",
             "zh-CN": "选择视频解码器",
+            "zh-TW": "選擇視頻解碼器",
+            "ja": "ビデオデコーダを選択",
         },
         "video_encoder_label": {
             "en": "Select video encoder",
@@ -508,6 +598,33 @@ function langsel(lang = null)
         document.getElementById('outputdir').placeholder = translations['tip']['ph003']['en'];
     }
 
+    if (translations['tip']['ph004'][lang])
+    {
+        document.getElementById('outputfilename').placeholder = translations['tip']['ph004'][lang];
+    }
+    else
+    {
+        document.getElementById('outputfilename').placeholder = translations['tip']['ph004']['en'];
+    }
+
+    if (translations['tip']['ph005'])
+    {
+        document.getElementById('videobyte').placeholder = translations['tip']['ph005'][lang];
+    }
+    else
+    {
+        document.getElementById('videobyte').placeholder = translations['tip']['ph005']['en'];
+    }
+
+    if (translations['tip']['ph006'][lang])
+    {
+        document.getElementById('videofps').placeholder = translations['tip']['ph006'][lang];
+    }
+    else
+    {
+        document.getElementById('videofps').placeholder = translations['tip']['ph006']['en'];
+    }
+
     if (translations['tip']['tip001'][lang])
     {
         document.getElementById('tip001').title = translations['tip']['tip001'][lang];
@@ -542,6 +659,87 @@ function langsel(lang = null)
     else
     {
         document.getElementById('tip004').title = translations['tip']['tip004']['en'];
+    }
+
+    if (translations['tip']['tip005'][lang])
+    {
+        document.getElementById('tip005').title = translations['tip']['tip005'][lang]
+    }
+    else
+    {
+        document.getElementById('tip005').title = translations['tip']['tip005']['en'];
+    }
+
+    if (translations['tip']['tip006'][lang])
+    {
+        document.getElementById('tip006').title = translations['tip']['tip006'][lang];
+    }
+    else
+    {
+        document.getElementById('tip006').title = translations['tip']['tip006']['en'];
+    }
+
+    if (translations['tip']['tip007'][lang])
+    {
+        document.getElementById('tip007').title = translations['tip']['tip007'][lang];
+    }
+    else
+    {
+        document.getElementById('tip007').title = translations['tip']['tip007']['en'];
+    }
+
+    if (translations['tip']['tip008'][lang])
+    {
+        document.getElementById('tip008').title = translations['tip']['tip008'][lang];
+    }
+    else
+    {
+        document.getElementById('tip008').title = translations['tip']['tip008']['en'];
+    }
+
+    if (translations['tip']['tip009'][lang])
+    {
+        document.getElementById('tip009').title = translations['tip']['tip009'][lang];
+    }
+    else
+    {
+        document.getElementById('tip009').title = translations['tip']['tip009']['en'];
+    }
+
+    if (translations['tip']['tip010'][lang])
+    {
+        document.getElementById('tip010').title = translations['tip']['tip010'][lang];
+    }
+    else
+    {
+        document.getElementById('tip010').title = translations['tip']['tip010']['en'];
+    }
+
+    if (translations['tip']['tip011'][lang])
+    {
+        document.getElementById('tip011').title = translations['tip']['tip011'][lang];
+    }
+    else
+    {
+        document.getElementById('tip011').title = translations['tip']['tip011']['en'];
+    }
+
+    if (translations['tip']['tip012'][lang])
+    {
+        document.getElementById('tip012').title = translations['tip']['tip012'][lang];
+    }
+    else
+    {
+        document.getElementById('tip012').title = translations['tip']['tip012']['en'];
+    }
+
+    if (translations['tip']['tip013'][lang])
+    {
+        document.getElementById('tip013').title = translations['tip']['tip013'][lang];
+    }
+    else
+    {
+        document.getElementById('tip013').title = translations['tip']['tip013']['en'];
     }
     // tip end
     // video

05f5601f958169ccee984f3243d961bacfbba1a3
Lingfengyu <1378990254@qq.com>
2024-01-10 12:25:53 +0800

    update to electron

--- src/main.js
+++ src/main.js
@@ -1,3 +1,87 @@
+// Modules to control application life and create native browser window
+const { app, shell, BrowserWindow } = require('electron');
+const path = require('path');
+const { electronApp, optimizer } = require('@electron-toolkit/utils');
+
+function createWindow()
+{
+    // Create the browser window.
+    const mainWindow = new BrowserWindow({
+        width: 900,
+        height: 670,
+        show: false,
+        autoHideMenuBar: true,
+        ...(process.platform === 'linux'
+            ? {
+                icon: path.join(__dirname, '../resources/icon.png')
+            }
+            : {}),
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            sandbox: false
+        }
+    });
+
+    mainWindow.on('ready-to-show', () =>
+    {
+        mainWindow.show();
+    });
+
+    mainWindow.on('show', () =>
+    {
+        translations_load();
+    })
+
+    mainWindow.webContents.setWindowOpenHandler((details) =>
+    {
+        shell.openExternal(details.url);
+        return { action: 'deny' };
+    });
+
+    // and load the index.html of the app.
+    mainWindow.loadFile(path.join(__dirname, 'index.html'));
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() =>
+{
+    // Set app user model id for windows
+    electronApp.setAppUserModelId('com.electron');
+
+    // Default open or close DevTools by F12 in development
+    // and ignore CommandOrControl + R in production.
+    // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
+    app.on('browser-window-created', (_, window) =>
+    {
+        optimizer.watchWindowShortcuts(window);
+    });
+
+    createWindow();
+
+    app.on('activate', function ()
+    {
+        // On macOS it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', function ()
+{
+    if (process.platform !== 'darwin')
+    {
+        app.quit();
+    }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
+const { translations } = require("./translations");
 function generatecommand()
 {
     // 输出命令的textarea
@@ -464,7 +548,7 @@ function startwebsocket(e)
             console.log("ws启动");
             var time = new Date();
             ws.send("start " + time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds());
-        }
+        };
         ws.onmessage = function (event)
         {
             console.log("ws收到消息" + event.data);
@@ -473,15 +557,15 @@ function startwebsocket(e)
                 var output = document.getElementById("wsState");
                 output.innerHTML = "运行命令完成";
             }
-        }
+        };
         ws.onerror = function (event)
         {
             console.log("ws报错" + event.data);
-        }
+        };
         ws.onclose = function (event)
         {
             console.log("ws关闭");
-        }
+        };
         ckws();
     }
     // send message to websocket
